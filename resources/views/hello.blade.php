7985dcbfb990da2d3e024e908b364df041bf9d6a
Domiciano <guilhermedomicianosilva@gmail.com>
2025-06-23 13:28:33 -0300

    Initial commit: Laravel Task Manager project setup and US2 completed

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá, Laravel</title>
+</head>
+<body>
+    <h1>Meu primeiro Olá, mundo com Laravel!</h1>
+    <p>E isso é uma view!</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Olá, mundo!';
+});
+
+Route::get('/helloview', function(){
+    return view('hello');
+});
